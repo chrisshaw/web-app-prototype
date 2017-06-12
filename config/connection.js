60718d2798335475536d4b0008268c970a9234dc
FH <FH@Fionas-Air.lan>
2017-06-11 21:41:05 -0400

    put google database settings back as they seem to have gotten lost:

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 // connect to the database
-var db_url = process.env.MONGODB_URI ||  'mongodb://localhost:27017/sidekick';
+// var db_url = process.env.MONGODB_URI ||  'mongodb://localhost:27017/sidekick';
+// mongodb://sidekick:sidekick2017:@ds113702.mlab.com:13702/sidekick
+
+var keys = require('./keys.js');
+var db_url = 'mongodb://' + keys.mongoUser+ ':' + keys.mongoPass + ':@' + keys.mongoHost + ':' + mongoPort + '/' + keys.mongoDatabase;
 var connection = mongoose.connect(db_url);
 // export the connection
 module.exports = connection;
