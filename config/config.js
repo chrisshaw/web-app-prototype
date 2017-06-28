150e0a29a08e2629d71170b0bf5e3b8470144255
FH <FH@Fionas-Air.lan>
2017-06-28 17:20:27 -0400

    checking db settings

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
     module.exports = {database: {
         name: 'skdb',
-        hostPort: 'http://146.148.55.53:8529/',
+        hostPort: '146.148.55.53:8529',
         un: 'dbadmin',
         pw: ''
     }}
