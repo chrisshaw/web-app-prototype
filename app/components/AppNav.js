49b741ccf0e60e5049f2d3fa672c6ef085049b3f
FH <FH@Fionas-Air.lan>
2017-07-16 14:07:44 -0400

    made nav bar menu on very small screens as it looks better

--- app/components/AppNav.js
+++ app/components/AppNav.js
@@ -27,6 +27,30 @@ const styles = {
 };
 
 
+var component = this;
+const Small = (props) => (
+  <IconMenu className="menu-on-small-screen"
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem  containerElement={<Link to="/buildpath"/>} primaryText="Build Paths" />
+    <MenuItem  primaryText="Manage Students" containerElement='label'  containerElement={<Link to="/managestudents"/>} />
+    <MenuItem  primaryText="Manage Users"  containerElement={<Link to="/manageusers"/>} />
+    <MenuItem   primaryText="Logout" onTouchTap={props.handleLogout} />
+  </IconMenu>
+);
+
+const Normal = (props) => (<div className="menu-on-normal-screen">
+                      <FlatButton  labelStyle={{lineHeight: 4}} style={{ borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6',maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement={<Link to="/buildpath"/>} label="Build Paths" />
+                      <FlatButton  labelStyle={{lineHeight: 4}} style={{borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6', maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement='label' label="Manage Students"  containerElement={<Link to="/managestudents"/>} />
+                      <FlatButton  labelStyle={{lineHeight: 4}} style={{borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6', maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement='label' label="Sign Up"  containerElement={<Link to="/manageusers"/>} />
+                      <IconButton  onTouchTap={props.handleLogout}  iconStyle={{height: 48}} style={{maxWidth: 100, alignSelf: 'center'}} ><LogoutIcon /></IconButton>
+                      </div>);
+
 class AppNav extends Component {
     constructor(props){
         super(props);
@@ -39,13 +63,9 @@ class AppNav extends Component {
                     iconStyleRight={{order: 4, marginTop: 0, marginLeft: 0, display: "flex"}}
                     iconStyleLeft={{display: 'none'}}
                     iconElementRight={<div>{this.props.loggedin ? 
-                      (<div>
-                      <FlatButton  labelStyle={{lineHeight: 4}} style={{ borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6',maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement={<Link to="/buildpath"/>} label="Build Paths" />
-                      <FlatButton  labelStyle={{lineHeight: 4}} style={{borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6', maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement='label' label="Manage Students"  containerElement={<Link to="/managestudents"/>} />
-                      <FlatButton  labelStyle={{lineHeight: 4}} style={{borderTop: 'solid 1px #E6E6E6',borderLeft: 'solid 1px #E6E6E6', borderRight: 'solid 1px #E6E6E6', maxWidth: 170, fontSize: 12, height: 50, flex: 'center', borderRadius: 0}} containerElement='label' label="Sign Up"  containerElement={<Link to="/manageusers"/>} />
-                      <IconButton  onTouchTap={this.props.handleLogout}  iconStyle={{height: 48}} style={{maxWidth: 100, alignSelf: 'center'}} ><LogoutIcon /></IconButton>
-                      </div>)  : (<div className="placeholder"></div>) } </div>}
-                    />            
+                      (<div><Small {...this.props} /><Normal {...this.props}/></div>)  : (<div className="placeholder"></div>) } </div>}
+                    />     
+           
                 </div>)
     }
 };
@@ -55,3 +75,7 @@ const mapStateToProps = (store) => {
     }
 }
 export default connect(mapStateToProps)(AppNav); 
+
+
+
+
