622fe081e0308cba9ec75a9482c9daba43502c8d
FH <FH@Fionas-Air.lan>
2017-07-17 22:58:58 -0400

    fixed uncontrolled compponent msg in google console

--- app/components/Password.js
+++ app/components/Password.js
@@ -14,6 +14,7 @@ class Password extends Component{
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.state = { 
+                    verify: '',
                     password : '', 
                     error: false, 
                     errorMsg: ""};
