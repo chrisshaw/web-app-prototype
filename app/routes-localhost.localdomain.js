5d55f3c572de4f5a7938d854edebddaaaf266a49
Chris <chris@localhost.localdomain>
2017-08-20 22:26:53 -0500

    migrated to linux laptop, fixing permissions issues

---
+++ app/routes-localhost.localdomain.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactRouter from "react-router";
+// import IndexRoute from "react-router";
+// import PropsRoute from "react-router";
+import Main from "./components/Main.js";
+import SignUpTab from "./components/SignUpTab";
+import LoginTab from "./components/LoginTab";
+import Forbidden from "./components/Forbidden";
+import Password from "./components/Password";
+import validatePerms  from "./components/ValidatePerms";
+import PathBuilder from "./components/PathBuilder.js";
+import DataImportStudentCSV from "./components/DataImportStudentCSV.js";
+import {Router, Route, hashHistory, browserHistory, IndexRoute} from "react-router";
+// import ValidatePermissions from "./components/ValidatePermissions.js";
+
+// i have added an auth prop to each route to indicate what perm item is needed to view
+// this currently matches the route name but does not need to as long as it matches a perm in the db auth_perms table
+var routes = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Main} > 
+            <IndexRoute auth='buildPath' component={validatePerms(PathBuilder)}/>
+            <Route path='/manageusers' auth='createAccounts' component={validatePerms(SignUpTab)} />
+            <Route path='/buildpath' auth='buildPath' component={validatePerms(PathBuilder)} /> 
+            <Route path='/uploadstudents'  auth='manageStudents' component={validatePerms(DataImportStudentCSV)} />
+             <Route path='/password' component={Password} />
+            <Route path='/forbidden' component={Forbidden} />
+            <Route path='/login' component={LoginTab} />
+        </Route>
+    </Router>
+)
+
+export default routes;
+
