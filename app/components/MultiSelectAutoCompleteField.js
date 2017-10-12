0aebb45de751684d527e03d7e0efbad599767b1d
Maria Khokhar <mak5793@live.com>
2017-10-12 14:01:48 -0500

    Added MultiSelectAutoCompleteField

---
+++ app/components/MultiSelectAutoCompleteField.js
@@ -0,0 +1,262 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Subheader from 'material-ui/Subheader';
+import Chip from 'material-ui/Chip';
+import MenuItem from 'material-ui/MenuItem';
+import AutoComplete from 'material-ui/AutoComplete';
+import {connect} from 'react-redux';
+
+import helper from '../helper';
+
+const ENTER_KEY = 'Enter';
+const TAB_KEY = 'Tab';
+const BACKSPACE_KEY = 'Backspace';
+const COMMA_KEY = ',';
+
+const styles = {
+  interestsWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  // categorySubheader: {
+  //   padding: null,
+  // },
+  interestChip: {
+    margin: '4px 4px 4px 0',
+    overflow: 'hidden',
+  },
+  interestChipLabel: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  interestInput: {
+    width: null,
+    flexGrow: 1,
+  },
+  interestInputHint: {
+    bottom: '10px',
+  },
+  interestInputUnderline: {
+    width: '3000px',
+    bottom: '3px',
+    left: '-1000px',
+    right: '-1000px',
+  },
+  interestInputUnderlineFocus: {
+    borderTop: 'rgb(244, 244, 244)',
+    borderBottom: 'rgb(244, 244, 244)',
+    borderLeft: 'rgb(244, 244, 244)',
+    borderRight: 'rgb(244, 244, 244)',
+  },
+  sectionWrapper: {
+    marginBottom: '20px',
+  },
+  dropDownMenu: {
+    maxHeight: '250px',
+    width: '350px',
+  },
+  dropDownList: {
+    width: '350px',
+  },
+  textField: {
+    width: '350px',
+  }
+};
+
+class MultiSelectAutoCompleteField extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextCustomId: props.options.length,
+      customSelections: [],
+      searchText: '',
+      menuItems: props.options.map(
+        function(option) {
+          return { "text" : option.name,
+                    "value" : (
+                      <MenuItem
+                      primaryText={option.name}
+                      insetChildren={true} />
+                    )};
+        }),
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputKeyDown = this.handleInputKeyDown.bind(this);
+    this.handleMenuItemSelected = this.handleMenuItemSelected.bind(this);
+    this.handleDeleteSelection = this.handleDeleteSelection.bind(this);
+  }
+
+  handleInputChange(newValue, dataSource, params) {
+    this.setState({ searchText: newValue });
+    console.log("input change: " + newValue);
+  }
+
+  getMenuItemIndex(option) {
+    if (option !== '') {
+      for (var index = 0; index < this.state.menuItems.length; index++) {
+        if (this.state.menuItems[index].text == option) {
+          return index;
+        }
+      }
+    }
+    return -1; //not found
+  }
+
+  addChipForSelectedOption(indexOfSelection) {
+    const searchText = this.state.searchText.trim();
+    if (searchText == '') {
+      return;
+    }
+
+    var optionAlreadySelected = this.props.selectedOptions.filter(function(obj) { return obj.name == searchText}).length > 0;// this.state.selectedOptions.filter(function(obj) { return obj.option == searchText }).length > 0;//
+    if (optionAlreadySelected) {
+      return;
+    }
+
+    // this.props.onAddInterest(this.props.category, searchText);
+    // helper.updateSelected(searchText, this.props.queryItem, this.props.dispatch);
+    // this.props.onAddSelection(searchText);
+
+    var suggestedOptionSelected = indexOfSelection > -1;
+    if (suggestedOptionSelected) {
+      helper.updateSelected(this.props.options[indexOfSelection], this.props.queryItem, this.props.dispatch);
+      this.setState((prevState) => ({
+        // selectedOptions: prevState.selectedOptions.concat({ "option" : searchText, "custom" : false }),
+        menuItems: prevState.menuItems.map(function(menuItem, index) {
+          if (index == indexOfSelection) {
+            return { "text" : menuItem.text,
+                      "value" : (
+                        <MenuItem
+                          primaryText={menuItem.text}
+                          checked={true}
+                          disabled={true} />)};
+          }
+          return menuItem;
+        }),
+        searchText: ''
+      }));
+    } else {
+      helper.updateSelected({ _id: this.state.nextCustomId, name: searchText}, this.props.queryItem, this.props.dispatch);
+      this.setState((prevState) => ({
+        nextCustomId: prevState.nextCustomId + 1,
+        // selectedOptions: prevState.selectedOptions.concat(searchText),//{ "option" : searchText, "custom" : true }),
+        customSelections: prevState.customSelections.concat(searchText),
+        searchText: ''
+      }));
+    }
+    console.log(this.state);
+    return;
+  }
+
+  handleInputKeyDown(event) {
+    const { key } = event;
+
+    if (key === ENTER_KEY || key === TAB_KEY || key === COMMA_KEY) {
+      event.preventDefault();
+
+      const searchText = this.state.searchText.trim();
+      var indexOfSelection = this.getMenuItemIndex(searchText);
+      if (indexOfSelection < 0) {
+        this.addChipForSelectedOption(indexOfSelection);
+      }
+
+      return;
+    }
+
+    if (key === BACKSPACE_KEY && this.state.searchText === '') {
+      // const { category } = this.props;
+      this.handleDeleteSelection(/*category,*/ this.props.selectedOptions.length - 1);
+    }
+  }
+
+  handleDeleteSelection(indexOfSelection) {
+    const { selectedOptions } = this.props;
+    this.setState((prevState) => ({
+      // selectedOptions: prevState.selectedOptions.filter(function(selection) { if (selection != prevState.selectedOptions[indexOfSelection]) { return selection; } }),
+      customSelections: prevState.customSelections.filter(function(selection) { if (selection != selectedOptions[indexOfSelection]) { return selection; } }),
+      menuItems: prevState.menuItems.map(function(menuItem, index) {
+        if (menuItem.text == selectedOptions[indexOfSelection].name) {
+          return { "text" : menuItem.text,
+                    "value" : (
+                      <MenuItem
+                        primaryText={menuItem.text}
+                        insetChildren={true} />)};
+        }
+        return menuItem;
+      }),
+    }));
+    helper.removeChip(/*id*/this.props.selectedOptions[indexOfSelection]._id, this.props.queryItem, this.props.dispatch);
+    // onRemoveSelection(indexOfSelection);
+    // onDeleteInterest(category, indexOfSelection);
+    console.log(this.state);
+  }
+
+  handleMenuItemSelected(chosenRequest, index) {
+    var indexOfSelection = index;
+    if (indexOfSelection < 0) {
+      indexOfSelection = this.getMenuItemIndex(chosenRequest)
+    }
+    if (indexOfSelection > -1) {
+      this.addChipForSelectedOption(indexOfSelection);
+    }
+    return;
+  }
+
+  render() {
+    // const { selectedOptions } = this.props;
+    return (
+      <div style={styles.sectionWrapper}>
+        <div style={styles.interestsWrapper}>
+          {this.props.selectedOptions.map((selectedOption, index) => (
+            <Chip style={styles.interestChip}
+                  labelStyle={styles.interestChipLabel}
+                  onRequestDelete={() => { this.handleDeleteSelection(/*category,*/ index); }}
+                  key={index}
+            >
+              {selectedOption.name}
+            </Chip>
+          ))}
+          <AutoComplete
+                    dataSource={this.state.menuItems}
+                    // dataSourceConfig={{text : 'option', value : 'value'}}
+           hintText={this.props.hintText}
+                     onUpdateInput={this.handleInputChange}
+                     onKeyDown={this.handleInputKeyDown}
+                     onNewRequest={this.handleMenuItemSelected}
+                     openOnFocus={true}
+                    filter={(searchText, key) => (searchText.trim() == '') || (key.indexOf(searchText) !== -1)}
+                     style={styles.interestInput}
+                     underlineStyle={styles.interestInputUnderline}
+                     underlineFocusStyle={styles.interestInputUnderlineFocus}
+                     hintStyle={styles.interestInputHint}
+                     searchText={this.state.searchText}
+                     listStyle={styles.dropDownList}
+                     menuStyle={styles.dropDownMenu}
+                     textFieldStyle={styles.textField}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+MultiSelectAutoCompleteField.propTypes = {
+  // category: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  selectedOptions: PropTypes.array.isRequired,
+  queryItem: PropTypes.string.isRequired,
+  // onAddSelection: PropTypes.func.isRequired,
+  // onRemoveSelection: PropTypes.func.isRequired,
+};
+
+MultiSelectAutoCompleteField.defaultProps = {
+  // category: '',
+  selectedOptions: [],
+  options: [],
+  queryItem: '',
+  hintText: '',
+};
+
+export default connect()(MultiSelectAutoCompleteField);
--- app/components/QueryBuilder.js
+++ app/components/QueryBuilder.js
@@ -8,6 +8,7 @@ import helper from '../helper';
 import {connect} from 'react-redux';
 import AutoCompleteField from './AutoCompleteField';
 import CourseSelection from './CourseSelection';
+import MultiSelectAutoCompleteField from './MultiSelectAutoCompleteField';
 import GradeSelection from './GradeSelection';
 import TopicSelection from './TopicSelection';
 import SubjectContentSelection from './SubjectContentSelection';
@@ -23,17 +24,33 @@ class QueryBuilder extends Component{
         // this.handleSend = this.handleSend.bind(this);
         this.handleClose = this.handleClose.bind(this);
         // get initial data and set props
+        // this.loadData(props);
         var searchObj = {};
         this.state ={
-            error: false, 
+            error: false,
             errorMsg: ""
         }
     }
-    handleClose(){ 
+
+    componentWillMount() {
+      helper.getGrades(false, false, this.props.dispatch);
+      helper.getTopics(false, false, this.props.dispatch);
+      console.log("QueryBuilder props: " + this.props);
+      helper.getCourses(false,false,  this.props.username, this.props.role, this.props.dispatch);
+      helper.getSubjectContents(false, false, this.props.dispatch);
+      helper.getStandards(false, false, "",this.props.dispatch);
+    }
+
+    // componentWillMount() {
+    //   console.log("will mount");
+    //   helper.getCourses(false,false,  this.props.username, this.props.role, this.props.dispatch);
+    // }
+
+    handleClose(){
         // local validation
         this.setState({error: false, errorMsg: ""});
     }
- 
+
     handleSubmitAll() {
         // clear message
         // array to store new paths
@@ -42,27 +59,27 @@ class QueryBuilder extends Component{
         var hasCourses = false;
         var selectedCourses = [];
         if (this.props.selectedcourselist) selectedCourses.push(this.props.selectedcourselist);
-        // A user will have no courses if courselist + selectedcouselist is empty 
+        // A user will have no courses if courselist + selectedcouselist is empty
         console.log("this.props.courselist", this.props.courselist)
         if ((this.props.courselist.length > 0) || (selectedCourses.length > 0)){
             hasCourses = true;
             if (((this.props.role.toUpperCase() === "TEACHER") || (this.props.role.toUpperCase() === "STUDENT")) && (selectedCourses.length === 0)){
                 // send back all the teachers course - so that not all course are queried only all the teachers / students courses
-                myCourses = Array.from(this.props.courselist);    
+                myCourses = Array.from(this.props.courselist);
             } else if (this.props.selectedcourselist) {
-                myCourses = Array.from(this.props.selectedcourselist); 
-            } 
-        } 
+                myCourses = Array.from(this.props.selectedcourselist);
+            }
+        }
         // messy logic will rework later!!!
         /// if all good then send the query
         if (hasCourses){
             console.log("this.props.selectedtopiclist", this.props.selectedtopiclist)
-            helper.getPathProjectAll(myCourses,this.props.selectedgradelist, this.props.selectedstandardslist, this.props.selectedtopiclist, this.props.selectedsubjectcontentlist, this.props.role, this.props.dispatch); 
+            helper.getPathProjectAll(myCourses,this.props.selectedgradelist, this.props.selectedstandardslist, this.props.selectedtopiclist, this.props.selectedsubjectcontentlist, this.props.role, this.props.dispatch);
             // reset display settings
             helper.addInitialRows("", this.props.dispatch);
         } else {
             this.setState({error: true , errorMsg:  "You have no COURSES set up in Sidekick, please contact your administrator."})
-        }  
+        }
     }
     render(){
         const actions = [
@@ -79,14 +96,14 @@ class QueryBuilder extends Component{
                 zIndex: 1500,
             }
         }
-    
+
         var component = this;
         return(<div className="drawer-wrapper">
                 <Row>
                     <Col xs={12} md={12} >
                         <div className="query-builder-wrapper">
                         <h3> Build Path </h3>
-                        <p> Enter grade and other filter criteria below, then submit to get reccommended paths. </p> 
+                        <p> Enter grade and other filter criteria below, then submit to get recommended paths. </p>
                         </div>
                     </Col>
                 </Row>
@@ -106,18 +123,93 @@ class QueryBuilder extends Component{
                         </Dialog> : " "}
                 </Col>
                 </Row>
-                <GradeSelection selectedgradelist={this.props.selectedgradelist}/>
-                <CourseSelection selectedcourselist={this.props.selectedcourselist} role={this.props.role} username={this.props.username}/>
-                <TopicSelection selectedtopiclist={this.props.selectedtopiclist}/>
-                <SubjectContentSelection selectedsubjectcontentlist={this.props.selectedsubjectcontentlist}/>
-                <StandardsSelection  selectedstandardslist={this.props.selectedstandardslist}/>              
+                <div>
+                  <Row>
+                    <Col xs={10} md={10} >
+                      <p className="search-text chip-float"> Students in grades</p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={12} md={12} >
+                      <MultiSelectAutoCompleteField
+                        options={this.props.gradelist}
+                        selectedOptions={this.props.selectedgradelist}
+                        queryItem="Grades"
+                        hintText="Select one or more student grade(s) e.g. 6" />
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                            <Row>
+                                <Col xs={10} md={10} >
+                                    <p className="search-text chip-float"> who are enrolled in</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} md={12} >
+                                <MultiSelectAutoCompleteField
+                                  options={this.props.courselist}
+                                  selectedOptions={this.props.selectedcourselist}
+                                  queryItem="Courses"
+                                  hintText="Select one or more course(s) e.g. Biology"/>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div>
+                                    <Row>
+                                        <Col xs={10} md={10} >
+                                            <p className="search-text chip-float">will explore topics</p>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={12} md={12} >
+                                            <MultiSelectAutoCompleteField
+                                              options={this.props.topiclist}
+                                              selectedOptions={this.props.selectedtopiclist}
+                                              queryItem="Topics"
+                                              hintText="Select one or more topic(s) e.g. immigration" />
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <div>
+                                            <Row>
+                                                <Col xs={10} md={10} >
+                                                    <p className="search-text chip-float">while learning content in</p>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col xs={12} md={12} >
+                                                <MultiSelectAutoCompleteField
+                                                options={this.props.subjectcontentlist}
+                                                selectedOptions={this.props.selectedsubjectcontentlist}
+                                                queryItem="Subjects"
+                                                hintText="Select one or more subject(s) e.g. english, math" />
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                        <div>
+                                                    <Row>
+                                                        <Col xs={10} md={10} >
+                                                            <p className="search-text chip-float">that aligns to standards</p>
+                                                        </Col>
+                                                    </Row>
+                                                    <Row>
+                                                        <Col xs={12} md={12} >
+                                                        <MultiSelectAutoCompleteField
+                                                          options={this.props.standardslist}
+                                                          selectedOptions={this.props.selectedstandardslist}
+                                                          queryItem="Standards"
+                                                          hintText="Select one or more standard(s)" />
+                                                        </Col>
+                                                    </Row>
+                                                </div>
                 <Row>
-                    <Col xs={12} md={12} className="text-center" >            
+                    <Col xs={12} md={12} className="text-center" >
                         <FlatButton containerElement='label' label="Get Recommended Paths" onTouchTap={this.handleSubmitAll} />
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs={12} md={12} className="text-center" >            
+                    <Col xs={12} md={12} className="text-center" >
                         { (this.props.perms) && (this.props.perms.indexOf('publish') !== -1) ? <FlatButton containerElement='label' label="Start Publishing to LMS" disabled={this.props.disabled} onTouchTap={this.props.handleSend} /> : ""}
                     </Col>
                 </Row>
@@ -128,19 +220,22 @@ class QueryBuilder extends Component{
 
 const mapStateToProps = (store) => {
     return {
+      role: store.authState.role,
+      username: store.authState.username,
+        gradelist: store.mainState.gradelist,
+        courselist: store.mainState.courselist,
+        topiclist: store.mainState.topiclist,
+        subjectcontentlist: store.mainState.subjectcontentlist,
+        standardslist: store.mainState.standardslist,
         selectedgrouplist: store.mainState.selectedgrouplist,
         selectedgradelist: store.mainState.selectedgradelist,
         selectedcourselist: store.mainState.selectedcourselist,
-        courselist: store.mainState.courselist,
         selectedtopiclist: store.mainState.selectedtopiclist,
-        selectedstandardslist: store.mainState.selectedstandardslist, 
+        selectedstandardslist: store.mainState.selectedstandardslist,
         selectedsubjectcontentlist: store.mainState.selectedsubjectcontentlist,
-        role: store.authState.role,
-        username: store.authState.username,
         disabled: store.mainState.disabled,
         perms:  store.authState.perms,
     }
 }
 
 export default connect(mapStateToProps)(QueryBuilder);
-
