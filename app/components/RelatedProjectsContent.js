63914f99aa47bf98c7657a23b1c54aa798acca04
Dunice Alliance <dunicealliance@Mac-mini-Dunice.local>
2017-11-15 10:53:34 +0300

    Make related project chips inline

--- app/components/RelatedProjectsContent.js
+++ app/components/RelatedProjectsContent.js
@@ -2,22 +2,27 @@ import React from 'react';
 import Chip from 'material-ui/Chip';
 
 const styles = {
-  interestChip: {
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  projectChip: {
     margin: '4px 4px 4px 0',
     overflow: 'hidden',
   },
-  interestChipLabel: {
+  projectChipLabel: {
     overflow: 'hidden',
     textOverflow: 'ellipsis',
   },
 };
 
 const RelatedProjectsContent = ({ items }) => (
-  <div>
+  <div style={styles.container}>
     {items.length > 0
       ? items.map((item, index) => (
-        <Chip style={styles.interestChip}
-              labelStyle={styles.interestChipLabel}
+        <Chip style={styles.projectChip}
+              labelStyle={styles.projectChipLabel}
               key={index}
         >
           {item}
